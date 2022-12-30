4c9cbd9ce6960224e808691a910eac4a98f8f239
Semir <sblekic1@etf.unsa.ba>
2022-12-30 16:30:42 +0100

    passing dummy data to templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Semir Blekic",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "December 30, 2022",
+    },
+    {
+        "author": "John Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "December 29, 2022",
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
