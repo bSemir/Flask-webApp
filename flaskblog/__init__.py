f84aff2cace2d2e8a6a2dfff5cc6f0a81977977a
Semir <sblekic1@etf.unsa.ba>
2023-01-02 21:56:30 +0100

    password hashing on registration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config["SECRET_KEY"] = "25fdd3c7d1e3e7462c36fdbeedbe66e7"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
