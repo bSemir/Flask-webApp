533c5e0332b9fd7b2e964bf7e47880532da5f6d8
Semir <sblekic1@etf.unsa.ba>
2023-01-03 00:45:17 +0100

    login users

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,8 +1,15 @@
 from datetime import datetime
-from flaskblog import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+# this is for reloading the user from the user ID stored in session,
+# and it needs to know how to find the user with that id, so we decorate it
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -25,3 +32,17 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+
+# the extension(login_manager) will expect user model to have certain atributes and methods
+# 4 to be specific(
+#   1. is_active is False
+#
+#   2. is_authenticated is False
+#
+#   3. is_anonymous is True
+#
+#   3. get_id() returns None
+# )
+# that's why we import UserMixin and inherit from to help us
+# https://flask-login.readthedocs.io/en/latest/
