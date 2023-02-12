a31f0cec25e3875db7fd4457d0e9e5d1811be97a
Semir <sblekic1@etf.unsa.ba>
2023-02-12 23:05:53 +0100

    functions for reset token

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 # this is for reloading the user from the user ID stored in session,
 # and it needs to know how to find the user with that id, so we decorate it
@@ -18,6 +19,20 @@ class User(db.Model, UserMixin):
     posts = db.relationship(
         "Post", backref="author", lazy=True
     )  # backref isn't the actual column in Post, but lets us access who is the user that created that post
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config["SECRET_KEY"], expires_sec)
+        # return the token that has user id as payload
+        return s.dumps({"user_id": self.id}).decode("utf-8")
+
+    @staticmethod  # bc we don't expect self as argument in this function
+    def verify_reset_token(token):
+        s = Serializer(app.config["SECRET_KEY"])
+        try:
+            user_id = s.loads(token)["user_id"]
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
